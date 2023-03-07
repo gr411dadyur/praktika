f882c80db2f3cdde984b8eb90ac6ff3c0331fa79
gr411dadyur <galattalantddy@gmail.com>
2023-03-07 15:52:05 +0700

    pr4

--- vue/js/main.js
+++ vue/js/main.js
@@ -7,7 +7,21 @@ let app = new Vue({
         altText:"A pair of socks",
         inventory: 100,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        onSale: true
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ],
+         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+         
+
 
     }
  })
