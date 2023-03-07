ffdc03972774a469a50203ac2eba1d16d88a5a24
gr411dadyur <galattalantddy@gmail.com>
2023-03-07 15:16:34 +0700

    pr2

---
+++ vue/js/main.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText:"A pair of socks",
+        inventory: 100,
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+
+    }
+ })
+ 
