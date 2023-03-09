bbefb76b1be02b98a26f9b8330eef2f9d5a113e7
gr411dadyur <galattalantddy@gmail.com>
2023-03-09 17:03:58 +0700

    pr7

--- vue/js/main.js
+++ vue/js/main.js
@@ -2,47 +2,72 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText:"A pair of socks",
+        selectedVariant: 0,
+        altText: "A pair of socks",
         inventory: 100,
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         onSale: false,
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
-
+                variantQuantity: 10,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
-         ],
-         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-         cart: 0,
-         inStock: false,
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
 
 
     },
+
     methods: {
         addToCart() {
             this.cart += 1
         },
         updateProduct(variantImage) {
             this.image = variantImage
-         },
-         delToCart() {
+        },
+        delToCart() {
             this.cart -= 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+         },
+         inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+         },
+         sale(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + ' Скидки!'
+            } else {
+                return  this.brand + ' ' + this.product + ' Нет скидок!'
+            }
+            
          }
-
+         
          
      }
-     
- })
 
-    
- 
+})
+
+
