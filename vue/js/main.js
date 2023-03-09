6e1b92f42567fecad4793f6bf8f22b1d3ed612ed
gr411dadyur <galattalantddy@gmail.com>
2023-03-09 18:18:15 +0700

    pr8

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,6 +1,54 @@
-let app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product', {
+    props: {
+        premium:{
+            type: Boolean,
+            required: true
+        }
+
+    },
+    template: `
+    <div class="product">
+     
+            <div class="product-image">
+                <img :src="image" :alt="altText" />
+
+            </div>
+     
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <a v-bind:href="link">More products like this</a>
+                <p v-if="inStock">In Stock</p>
+                <p v-else :class="{isNotStock: !inStock}">Out of Stock</p>
+                <p>{{ sale }}</p>
+                <p>Shipping: {{ shipping }}</p>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                 </ul>
+                 <div
+                     class="color-box"
+                     v-for="(variant, index) in variants"
+                     :key="variant.variantId"
+                     :style="{ backgroundColor:variant.variantColor }"
+                     @mouseover="updateProduct(index)"
+                >
+                </div>
+                 
+                 <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                 </ul>
+                 <div class="cart">
+                     <p>Cart({{ cart }})</p>
+                    </div>
+                    <button 
+                    v-on:click="addToCart"
+                    :disabled="!inStock" 
+                    :class="{ disabledButton: !inStock }">Add to cart</button>
+                    <button v-on:click="delToCart">Del to cart</button>
+            </div>
+        </div>
+  `,
+  data(){
+    return{
         product: "Socks",
         brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
@@ -27,47 +75,74 @@ let app = new Vue({
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
-
-
+    }
+  },
+  methods: {
+    addToCart() {
+        this.cart += 1
     },
-
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
-        delToCart() {
-            this.cart -= 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
+    updateProduct(variantImage) {
+        this.image = variantImage
+    },
+    delToCart() {
+        this.cart -= 1
+    },
+    updateProduct(index) {
+        this.selectedVariant = index;
+        console.log(index);
+    }
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product;
+    },
+    image() {
+        return this.variants[this.selectedVariant].variantImage;
+     },
+     inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+     },
+     sale(){
+        if(this.onSale){
+            return this.brand + ' ' + this.product + ' Скидки!'
+        } else {
+            return  this.brand + ' ' + this.product + ' Нет скидок!'
         }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-         },
-         inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-         },
-         sale(){
-            if(this.onSale){
-                return this.brand + ' ' + this.product + ' Скидки!'
-            } else {
-                return  this.brand + ' ' + this.product + ' Нет скидок!'
-            }
-            
-         }
-         
-         
+        
+     },
+     shipping() {
+        if (this.premium) {
+            return "Free";
+        } else {
+            return 2.99
+        }
+     }
+     
+     
      }
 
-})
+
+ }),
+ Vue.component('product-details', {
+    props: {
+      details: {
+        type: Array,
+        required: true
+      }
+    },
+    template: `
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+    `
+  })
+ 
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true,
+    }
+    })
+
 
 
